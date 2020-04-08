13dd511de9240559c8507d49eeebabcf93b7abc9
artofnext <novnovalex@gmail.com>
2020-04-08 14:09:38 +0300

    fontawesome work, cleanup

--- footer.php
+++ footer.php
@@ -38,7 +38,11 @@
           <h3 class="headline headline--small">Connect With Us</h3>
           <nav>
             <ul class="min-list social-icons-list group">
-              <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li>
+                <a href="#" class="social-color-facebook">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
               <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
               <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
               <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
--- header.php
+++ header.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <script src="https://use.fontawesome.com/8125038d06.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" 
-    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <?php wp_head(); ?>
-
     </head>
     <body>
     <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="#"><strong>Fictional</strong> University</a></h1>
-      <span class="js-search-trigger site-header__search-trigger"><i class="fab fa-search" aria-hidden="true"></i></span>
-      <i class="site-header__menu-trigger fab fa-bars" aria-hidden="true"></i>
-      
-      
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
 
       <div class="site-header__menu group">
         <nav class="main-navigation">
