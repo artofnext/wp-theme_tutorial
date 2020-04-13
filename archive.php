4e9ee0b494e16ceae90deb9f7be99792b3d238d3
artofnext <novnovalex@gmail.com>
2020-04-13 11:34:00 +0300

    archive title function

--- archive.php
+++ archive.php
@@ -7,14 +7,15 @@ get_header();
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">
-    <?php 
+    <?php the_archive_title() ?>
+    <!-- <?php 
     if (is_category()) {
         single_cat_title();
     }
     if (is_author()) {
         echo "Posts by "; the_author();
     } 
-    ?>
+    ?> -->
     </h1>
     <div class="page-banner__intro">
       <p>Latest news</p>
