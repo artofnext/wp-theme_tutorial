07d28e8afaeeaa862f247eecbdd9ca0c65cc3320
artofnext <novnovalex@gmail.com>
2020-04-13 11:26:20 +0300

    archive page for categorized posts

--- archive.php
+++ archive.php
@@ -6,7 +6,16 @@ get_header();
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to blog</h1>
+    <h1 class="page-banner__title">
+    <?php 
+    if (is_category()) {
+        echo "category name";
+    }
+    if (is_author()) {
+        echo "author name";
+    } 
+    ?>
+    </h1>
     <div class="page-banner__intro">
       <p>Latest news</p>
     </div>
